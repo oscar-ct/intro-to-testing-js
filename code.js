cc8ee5a059700775f26b44630ac84c23944000e6
Oscar Castro <oscar.a.castro818@gmail.com>
2022-02-07 09:48:01 -0600

    add test and ability to say hello to any input

--- code.js
+++ code.js
@@ -3,13 +3,20 @@ function helloWorld() {
     return "Hello, World!";
 }
 
+// function sayHello(input) {
+//     if ( input === "Alex") {
+//         return "Hello, Alex!";
+//     } else if ( input === "Pat") {
+//         return "Hello, Pat!"
+//     } else {
+//         return "Hello, Jane!";
+//     }
+// }
+
 function sayHello(input) {
-    if ( input === "Alex") {
-        return "Hello, Alex!";
-    } else if ( input === "Pat") {
-        return "Hello, Pat!"
-    } else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 }
+
+
+
 
